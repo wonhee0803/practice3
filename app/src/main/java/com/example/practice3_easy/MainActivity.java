e37b301f077f945e5647f444c2d1731409f8a6ec
Wonhee Park <wonhee0803@gmail.com>
2021-10-04 22:53:34 +0900

    practice3

---
+++ app/src/main/java/com/example/practice3_easy/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.practice3_easy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.practice3_easy.databinding.ActivityMainBinding;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private boolean checked = false;
+    private final Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> {
+            if (checked) {
+                Toast.makeText(this, "한번만 할 수 있음", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            checked = true;
+            int score = random.nextInt(101);
+            binding.score.setText(String.format("%d점", score));
+        });
+    }
+}
